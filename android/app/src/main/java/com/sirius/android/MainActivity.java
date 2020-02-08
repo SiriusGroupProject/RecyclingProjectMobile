9b0550ba01e2ba69d5e6a1fb9958327abaf39c6c
gizemelove <gizemelove06@gmail.com>
2020-02-08 13:30:32 +0300

    Balance page created and get request handled.

--- android/app/src/main/java/com/sirius/android/MainActivity.java
+++ android/app/src/main/java/com/sirius/android/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sirius.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, UserBalance.class);
+        startActivity(intent);
+
     }
+
 }
