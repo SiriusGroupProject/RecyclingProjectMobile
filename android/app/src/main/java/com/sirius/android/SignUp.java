6fcf8077d54d280e7a79b8e6b695df162cd0eb2d
gizemelove <gizemelove06@gmail.com>
2020-02-29 16:46:22 +0300

    Two pages updated.

--- android/app/src/main/java/com/sirius/android/SignUp.java
+++ android/app/src/main/java/com/sirius/android/SignUp.java
@@ -1,10 +1,12 @@
 package com.sirius.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +29,7 @@ public class SignUp extends AppCompatActivity {
     private String url = "http://172.20.10.10:8080/rest/users/addUser"; // ip edit
     private Button signUpButton;
     private EditText nameText, surnameText, emailText, passwordText;
+    private TextView signInLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +44,19 @@ public class SignUp extends AppCompatActivity {
         surnameText = (EditText) findViewById(R.id.surname);
         emailText = (EditText) findViewById(R.id.email);
         passwordText = (EditText) findViewById(R.id.password);
-        signUpButton = (Button) findViewById(R.id.buttonSignUp);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        signInLink = (TextView) findViewById(R.id.sign_in_account_link);
     }
 
     private void sendAndRequestResponse() {
 
+        signInLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+            }
+        });
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
